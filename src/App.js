bb3380ce11cdf12f48c7df1e487fd97052105263
shiroya krushil <sdcard1130@gmail.com>
2023-06-03 19:40:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { ref } from "yup";
+import * as yup from 'yup'
 function App() {
+
+  const defaultvalue = {
+    email: '',
+    phone: '',
+    password: '',
+    cpassword: ''
+  }
+
+
+
+  const validationSchema = yup.object().shape({
+    email: yup.string().email().required('Please enter Email address'),
+    phone: yup.number().required('Please enter phone number'),
+    password: yup.string().required("Please enter a password").min(8, "Password must have at least 8 characters"),
+    cpassword: yup.string().required('Please re-type your password').oneOf([ref("password")], "Passwords does not match"),
+  })
+
+  const handlesubmit = (value) => {
+    console.log('values', value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container p-5">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-lg-12 col-xl-11">
+            <div className="card text-black" style={{ borderradius: " 25px" }}>
+              <div className="card-body  p-md-5">
+                <div className="row justify-content-center">
+                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 mb-5">
+                      Sign up
+                    </p>
+                    <Formik initialValues={defaultvalue} validationSchema={validationSchema} onSubmit={handlesubmit}>
+                      <Form>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <label
+                              className="form-label "
+                              htmlFor="form3Example4c"
+                            >
+                              Email
+                            </label>
+                            <Field
+                              type="email"
+                              id="email"
+                              name="email"
+                              className={`form-control`}
+                            />
+                            <span className='text-danger'><ErrorMessage name='email' /></span>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <label
+                              className="form-label "
+                              htmlFor="form3Example4cd"
+                            >
+                              Phone
+                            </label>
+                            <Field
+                              type="text"
+                              id="phone"
+                              name="phone"
+                              className="form-control"
+                            />
+                            <span className='text-danger'><ErrorMessage name='phone' /></span>
+                          </div>
+                        </div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example4cd"
+                            >
+                              Password
+                            </label>
+                            <Field
+                              type="password"
+                              id="password"
+                              name="password"
+                              className="form-control"
+                            />
+                            <span className='text-danger'><ErrorMessage name='password' /></span>
+                          </div>
+                        </div>
+                        <div className="d-flex flex-row align-items-center mb-5">
+                          <div className="form-outline flex-fill mb-0">
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example4cd"
+                            >
+                              CPassword
+                            </label>
+                            <Field
+                              type="password"
+                              id="cpassword"
+                              name="cpassword"
+                              className="form-control"
+                            />
+                            <span className='text-danger'><ErrorMessage name='cpassword' /></span>
+                          </div>
+                        </div>
+
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4 ">
+                          <button type='submit' className='btn btn-primary'>Sign Up</button>
+                        </div>
+
+                      </Form>
+                    </Formik>
+                  </div>
+                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      className="img-fluid"
+                      alt="Sample image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
